6cd31f35642b67f1fe04a11e8146835e8d856811
ixguza <rtk0253@gmail.com>
2018-07-22 22:24:52 +0900

    goimports

--- installs.sh
+++ installs.sh
@@ -58,6 +58,7 @@ go get -u github.com/motemen/gore
 go get -u github.com/nsf/gocode
 go get -u github.com/k0kubun/pp
 go get -u golang.org/x/tools/cmd/godoc
+go get -u golang.org/x/tools/cmd/goimports
 
 ##################################################
 ##### 色々 #####
